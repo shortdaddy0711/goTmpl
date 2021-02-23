0dbbeb909b3b2bcbfffb0c6004e93f4f252408f3
shortdaddy0711 <sodoatm@gmail.com>
2021-02-22 16:57:26 -0800

    first commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+// User -
+type User struct {
+	Name  string
+	Email string
+	Age   int
+}
+
+func main() {
+	user1 := User{Name: "namsoo", Email: "namsoo@gmail.com", Age: 41}
+	user2 := User{Name: "olivia", Email: "olivia@gmail.com", Age: 8}
+	tmpl, err := template.New("users").Parse("Name: {{.Name}}\nEmail: {{.Email}}\nAge: {{.Age}}\n")
+	if err != nil {
+		panic(err)
+	}
+	tmpl.Execute(os.Stdout, user1)
+	tmpl.Execute(os.Stdout, user2)
+
+}
